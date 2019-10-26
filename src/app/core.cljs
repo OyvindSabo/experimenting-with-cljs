717eba86d9cad5bf2689c68697ef566f60bf74e8
oyvindsabo <oyvind.sabo@gmail.com>
2019-10-26 15:30:46 +0200

    Make it possible to select menus

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,8 +1,35 @@
 (ns app.core
   (:require
-    [reagent.core :as r]))
+   [reagent.core :as r]))
 
-(defn app [] [:div "Øyvind Sæbø"])
+(defn section-container
+  [children]
+  [:div
+   {:style
+    {:border "2px dashed #dcdde1"
+     :display "flex"
+     :margin "20px auto"
+     :padding "20px"
+     :width "1024px"}}
+   children])
+
+(defn title [children] [:h1 {:style {:margin "auto"}} children])
+
+(def selected-menu (r/atom 0))
+
+(defn menu-item [props & children]
+  [:div {:style {:flex-grow "1" :text-align "center"}}
+   [:span {:on-click (:on-click props) :style {:color "#487eb0" :cursor "pointer"}}
+    children]])
+
+(defn app []
+  [:div
+   [section-container [title "Øyvind Sæbø"]]
+   [section-container
+    [:<>
+     [menu-item {:on-click #(swap! selected-menu (fn [] 0))} "menu item 1"]
+     [menu-item {:on-click #(swap! selected-menu (fn [] 1))} "menu item 2"]]]
+   [section-container (case @selected-menu 0 "Menu item 1 selected" 1 "Menu item 2 seleceted")]])
 
 (defn ^:dev/after-load start
   []
